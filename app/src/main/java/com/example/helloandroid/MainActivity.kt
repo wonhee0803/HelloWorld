057ae94c20c3225088f4f31cbc12dcefcee91913
Wonhee Park <wonhee0803@gmail.com>
2021-09-06 14:46:25 +0900

    버튼

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun onClick(view: android.view.View) {
+        Toast.makeText( this, "안녕하세요 버튼 클릭입니다", Toast.LENGTH_LONG).show()
     }
 }
